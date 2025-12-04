6aad3c041a5cf990c024f9e118a2b24a8a5d22c1
Subashkannan R <137408679+Subash-Kannan-R@users.noreply.github.com>
2025-12-04 18:11:49 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose';
 
 export const connectDB = async () => {
-  const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://subashkannan0309_db_user:YOUR_PASSWORD@cluster0.knjnkt5.mongodb.net/tshirtDB?retryWrites=true&w=majority&appName=Cluster0
-';
+  const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://subashkannan0309_db_user:YOUR_PASSWORD@cluster0.knjnkt5.mongodb.net/tshirtDB?retryWrites=true&w=majority&appName=Cluster0';
   try {
     await mongoose.connect(MONGO_URI, {
       autoIndex: true
